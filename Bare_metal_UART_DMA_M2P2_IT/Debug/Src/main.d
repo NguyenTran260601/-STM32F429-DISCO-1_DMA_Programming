9260b8950accdd40c377815af43e870277447935
NguyenTran260601 <tranvannguyen2606@gmail.com>
2023-03-27 15:05:07 +0700

    Bare-metal programming UART transmit data DMA_IT
    Mode: M2P
    - UART3
    - DMA1_Stream3
    - Interrupt mechanism

---
+++ Bare_metal_UART_DMA_M2P2_IT/Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c ../Inc/main.h ../Inc/stm32f429xx.h \
+ D:/Embedded_C/My_workspace/DMA_Workspaces/Bare_metal_UART_DMA_M2P/Inc/CMSIS/core_cm4.h \
+ D:/Embedded_C/My_workspace/DMA_Workspaces/Bare_metal_UART_DMA_M2P/Inc/CMSIS/cmsis_version.h \
+ D:/Embedded_C/My_workspace/DMA_Workspaces/Bare_metal_UART_DMA_M2P/Inc/CMSIS/cmsis_compiler.h \
+ D:/Embedded_C/My_workspace/DMA_Workspaces/Bare_metal_UART_DMA_M2P/Inc/CMSIS/cmsis_gcc.h \
+ D:/Embedded_C/My_workspace/DMA_Workspaces/Bare_metal_UART_DMA_M2P/Inc/CMSIS/mpu_armv7.h \
+ ../Inc/it.h
+../Inc/main.h:
+../Inc/stm32f429xx.h:
+D:/Embedded_C/My_workspace/DMA_Workspaces/Bare_metal_UART_DMA_M2P/Inc/CMSIS/core_cm4.h:
+D:/Embedded_C/My_workspace/DMA_Workspaces/Bare_metal_UART_DMA_M2P/Inc/CMSIS/cmsis_version.h:
+D:/Embedded_C/My_workspace/DMA_Workspaces/Bare_metal_UART_DMA_M2P/Inc/CMSIS/cmsis_compiler.h:
+D:/Embedded_C/My_workspace/DMA_Workspaces/Bare_metal_UART_DMA_M2P/Inc/CMSIS/cmsis_gcc.h:
+D:/Embedded_C/My_workspace/DMA_Workspaces/Bare_metal_UART_DMA_M2P/Inc/CMSIS/mpu_armv7.h:
+../Inc/it.h:
